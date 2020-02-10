085877523247b0603e43027e860b00367ca509e6
matthewshan <matthewshan99@gmail.com>
2020-02-10 11:56:03 -0500

    Worked on client command line

--- client.py
+++ client.py
@@ -1,10 +1,41 @@
 import socket
+import sys
 
-HOST = '127.0.0.1'
-PORT = 21
+class Connection():
+    def __init__(self, ip, port):
+        self.HOST = ip
+        self.PORT = port
+        self.conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.conn.connect((HOST, PORT))
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    data = s.recv(1024)
+    def test(self):
+        data = s.recv(1024)
+        s.close()
 
-print(str(data))
+        print(data.decode("utf-8"))
+
+done = False
+socket = None
+while not done:
+    if(len(sys.argv) < 2):
+        print("Invalid command, please try again")
+    else:
+        command = sys.argv[1]
+        if command == "CONNECT":
+            socket = Connection(sys.argv[2], sys.argv[3])
+        elif command == "LIST":
+            print("list")
+        elif command == "RETRIEVE":
+            print("retrieve")
+        elif command == "STORE":
+            print("store")
+        elif command == "QUIT":
+            print("quit")
+        elif command == "TEST":
+            socket.test()
+        else:
+            print("Invalid command")
+
+
+
+
