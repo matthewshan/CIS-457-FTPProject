c7dfbbdb1cd005e00fee384eacda9bd1f204e243
matthewshan <matthewshan99@gmail.com>
2020-02-10 12:12:05 -0500

    Basic command line interface working

--- client.py
+++ client.py
@@ -2,40 +2,46 @@ import socket
 import sys
 
 class Connection():
-    def __init__(self, ip, port):
+    def __init__(self):
+        self.connected = False
+
+    def connect(self, ip, port):
+        self.connected = True
         self.HOST = ip
         self.PORT = port
         self.conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.conn.connect((HOST, PORT))
+        self.conn.connect((self.HOST, self.PORT))
 
     def test(self):
-        data = s.recv(1024)
-        s.close()
+        data = self.conn.recv(1024)
+        self.conn.close()
 
         print(data.decode("utf-8"))
+    
+    def close(self):
+        self.conn.close()
 
 done = False
-socket = None
-while not done:
-    if(len(sys.argv) < 2):
+connection = Connection()
+while True:
+    commands = str(raw_input("ftp> "))
+    commands = commands.split(" ")
+    if(len(commands) == 0):
         print("Invalid command, please try again")
     else:
-        command = sys.argv[1]
-        if command == "CONNECT":
-            socket = Connection(sys.argv[2], sys.argv[3])
-        elif command == "LIST":
+        if commands[0] == "CONNECT":
+            connection.connect(commands[1], int(commands[2]))
+            print("Connection Formed with " + commands[1] + ":" + commands[2])
+        elif commands[0] == "LIST":
             print("list")
-        elif command == "RETRIEVE":
+        elif commands[0] == "RETRIEVE":
             print("retrieve")
-        elif command == "STORE":
+        elif commands[0] == "STORE":
             print("store")
-        elif command == "QUIT":
-            print("quit")
-        elif command == "TEST":
-            socket.test()
+        elif commands[0] == "QUIT":
+            connection.close()
+            break
+        elif commands[0] == "TEST":
+            connection.test()
         else:
             print("Invalid command")
-
-
-
-
