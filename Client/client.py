0bb123c5536339c238a2fc8bad005d36c8389ddc
matthewshan <matthewshan99@gmail.com>
2020-02-16 15:27:02 -0500

    Working in File sockets

---
+++ Client/client.py
@@ -0,0 +1,71 @@
+import socket
+import sys
+import os
+
+class Connection():
+    def __init__(self):
+        self.connected = False
+
+    def connect(self, ip, port):
+        self.connected = True
+        self.HOST = ip
+        self.PORT = port
+        self.conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.conn.connect((self.HOST, self.PORT))
+
+    def test(self):
+        self.conn.sendall("This is a test".encode())
+        data = self.conn.recv(1024)
+
+        print(data.decode("utf-8"))
+
+    def list_files(self):
+        self.conn.sendall("LIST".encode())
+        print(self.conn.recv(1024).decode())
+        print(self.conn.recv(1024).decode())
+    
+    def close(self):    
+        self.conn.sendall("QUIT".encode())
+        print(self.conn.recv(1024).decode())
+        print(self.conn.recv(1024).decode())
+        self.conn.close()
+    
+    def retrieve(self, filename):
+        self.conn.sendall(("RETRIEVE " + filename).encode())
+        #TODO: Retrieve file
+
+    def store(self, filename):
+        size = os.path.getsize("./" + filename)
+        self.conn.sendall(("STORE " + filename  + " " + str(size)).encode())
+        my_file = open(filename, 'rb')
+        
+        self.conn.sendall(())
+        #TODO: Send the file
+        
+
+connection = Connection()
+while True:
+    commands = str(input("\nftp> "))
+    commands = commands.split(" ")
+    if(len(commands) == 0):
+        print("Invalid command, please try again")
+    else:
+        if commands[0] == "CONNECT":
+            if len(commands) == 1:
+                commands.append("localhost")
+                commands.append("21")
+            connection.connect(commands[1], int(commands[2]))
+            print("Connection Formed with " + commands[1] + ":" + commands[2])
+        elif commands[0] == "LIST":
+            connection.list_files()
+        elif commands[0] == "RETRIEVE":
+            connection.retrieve(commands[1])
+        elif commands[0] == "STORE":
+            connection.store(commands[1])
+        elif commands[0] == "QUIT":
+            connection.close()
+            break
+        elif commands[0] == "TEST":
+            connection.test()
+        else:
+            print("Invalid command")
---
+++ Server/server.py
@@ -0,0 +1,60 @@
+import socket
+import os
+
+HOST = '127.0.0.1'
+PORT = 21
+
+# THIS IS THE CORRECT FILE
+
+
+# Used for RETRIEVE
+def send_file(connection, filename):
+    pass
+
+# Used for STORE
+def receive_file(connection, filename, filesize):
+    pass
+
+# Used for LIST
+def list_files(connection):
+    cwd = os.getcwd()
+    files = os.listdir(cwd)
+    toSend = "====Files on Server====\n"
+    toSend += "\n".join(files)
+    toSend += "\n======================="
+    connection.sendall(toSend.encode())
+
+
+# Do the work of creating a connection and receiving commands
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen()
+    print("Server Listening")
+    connection, client = s.accept()
+    with connection:
+        while(True):
+            rData = connection.recv(1024).decode()
+            connection.sendall(("Received command: " + rData).encode())
+    
+            # If the command given is QUIT, send a goodbye to the client and terminate the connection
+            if rData == "QUIT":
+                connection.sendall("Bye bye".encode())
+                break
+            # If the command given is LIST, execute our list_files function
+            elif rData == "LIST":
+                list_files(connection)
+                continue 
+            # Differentiate between STORE and RECEIVE
+            rData = rData.split(" ")
+
+            # If the command given is STORE, get ready to accept a file
+            if rData[0] == "STORE":
+                receive_file(connection, rData[1], rData[2])
+            elif rData[0] == "RETRIEVE":
+                send_file(connection, rData[1])
+            else:
+                connection.sendall("Invalid command! Valid commands: QUIT, LIST, STORE, RETRIEVE".encode())
+            print("Client sent: ", rData)
+            # connection.sendall(rData.encode())
+            #print("Connection Recieved from:", client)
+            #connection.sendall("Hello World".encode())
